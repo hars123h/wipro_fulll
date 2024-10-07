0f66d948e2b2ff8b7d2f3d02ac7680f143d21987
Raman Madhukar <146058279+007raman@users.noreply.github.com>
2024-10-07 16:07:34 +0530

    api url

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -5,12 +5,11 @@ import p2 from '../images/img301.jpg'
 import { Link } from 'react-router-dom'
 import { LiaAngleLeftSolid } from 'react-icons/lia'
 import img1 from '../images/img1.jpg'
-import img2 from '../images/img2.jpg'
 import img3 from '../images/img3.jpg'
 import img4 from '../images/img4.jpg'
 import img5 from '../images/img5.jpg'
 import img6 from '../images/img6.jpg'
-import img7 from '../images/img7.jpg'
+import img2 from '../images/img7.jpg'
 
 const Article = () => {
     return (
